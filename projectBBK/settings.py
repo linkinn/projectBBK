eb7712d76755040b3e7e89fe65d3fe00a8f7b520
Fillipi <fillipi.nascimento@oihandover.com>
2020-02-20 17:14:05 -0300

    reafactor: alter achives

--- projectBBK/settings.py
+++ projectBBK/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['44.225.25.121', '*']
 
 
 # Application definition
